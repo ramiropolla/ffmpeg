62e9a8760a8b36b50b3431e5c70511079dc94b05
Ramiro Polla <ramiro.polla@gmail.com>
2024-06-06 18:33:34 +0200

    swscale/x86: add sse4 {lum,chr}ConvertRange
    chrRangeFromJpeg_8_c: 19.9
    chrRangeFromJpeg_8_sse4: 16.2
    chrRangeFromJpeg_24_c: 60.7
    chrRangeFromJpeg_24_sse4: 28.9
    chrRangeFromJpeg_128_c: 325.7
    chrRangeFromJpeg_128_sse4: 160.2
    chrRangeFromJpeg_144_c: 364.2
    chrRangeFromJpeg_144_sse4: 194.9
    chrRangeFromJpeg_256_c: 630.7
    chrRangeFromJpeg_256_sse4: 337.4
    chrRangeFromJpeg_512_c: 1240.4
    chrRangeFromJpeg_512_sse4: 668.4
    chrRangeToJpeg_8_c: 37.7
    chrRangeToJpeg_8_sse4: 19.7
    chrRangeToJpeg_24_c: 114.7
    chrRangeToJpeg_24_sse4: 30.2
    chrRangeToJpeg_128_c: 636.4
    chrRangeToJpeg_128_sse4: 161.7
    chrRangeToJpeg_144_c: 715.7
    chrRangeToJpeg_144_sse4: 272.9
    chrRangeToJpeg_256_c: 1256.7
    chrRangeToJpeg_256_sse4: 341.9
    chrRangeToJpeg_512_c: 2498.7
    chrRangeToJpeg_512_sse4: 668.4
    lumRangeFromJpeg_8_c: 11.7
    lumRangeFromJpeg_8_sse4: 12.4
    lumRangeFromJpeg_24_c: 36.9
    lumRangeFromJpeg_24_sse4: 17.7
    lumRangeFromJpeg_128_c: 228.4
    lumRangeFromJpeg_128_sse4: 85.2
    lumRangeFromJpeg_144_c: 272.9
    lumRangeFromJpeg_144_sse4: 96.9
    lumRangeFromJpeg_256_c: 463.4
    lumRangeFromJpeg_256_sse4: 183.9
    lumRangeFromJpeg_512_c: 879.9
    lumRangeFromJpeg_512_sse4: 355.9
    lumRangeToJpeg_8_c: 17.7
    lumRangeToJpeg_8_sse4: 15.4
    lumRangeToJpeg_24_c: 56.2
    lumRangeToJpeg_24_sse4: 18.4
    lumRangeToJpeg_128_c: 331.4
    lumRangeToJpeg_128_sse4: 84.4
    lumRangeToJpeg_144_c: 375.2
    lumRangeToJpeg_144_sse4: 96.9
    lumRangeToJpeg_256_c: 649.7
    lumRangeToJpeg_256_sse4: 184.4
    lumRangeToJpeg_512_c: 1281.9
    lumRangeToJpeg_512_sse4: 355.9

---
+++ libswscale/x86/range_convert.asm
@@ -0,0 +1,130 @@
+;******************************************************************************
+;* Copyright (c) 2024 Ramiro Polla
+;*
+;* This file is part of FFmpeg.
+;*
+;* FFmpeg is free software; you can redistribute it and/or
+;* modify it under the terms of the GNU Lesser General Public
+;* License as published by the Free Software Foundation; either
+;* version 2.1 of the License, or (at your option) any later version.
+;*
+;* FFmpeg is distributed in the hope that it will be useful,
+;* but WITHOUT ANY WARRANTY; without even the implied warranty of
+;* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;* Lesser General Public License for more details.
+;*
+;* You should have received a copy of the GNU Lesser General Public
+;* License along with FFmpeg; if not, write to the Free Software
+;* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;******************************************************************************
+
+%include "libavutil/x86/x86util.asm"
+
+SECTION_RODATA
+
+chr_to_mult:        times 4 dd 4663
+chr_to_offset:      times 4 dd -9289992
+%define chr_to_shift 12
+
+chr_from_mult:      times 4 dd 1799
+chr_from_offset:    times 4 dd 4081085
+%define chr_from_shift 11
+
+lum_to_mult:        times 4 dd 19077
+lum_to_offset:      times 4 dd -39057361
+%define lum_to_shift 14
+
+lum_from_mult:      times 4 dd 14071
+lum_from_offset:    times 4 dd 33561947
+%define lum_from_shift 14
+
+SECTION .text
+
+; NOTE: there is no need to clamp the input when converting to jpeg range
+;       (like we do in the C code) because packssdw will saturate the output.
+
+;-----------------------------------------------------------------------------
+; lumConvertRange
+;
+; void ff_lumRangeToJpeg_<opt>(int16_t *dst, int width);
+; void ff_lumRangeFromJpeg_<opt>(int16_t *dst, int width);
+;
+;-----------------------------------------------------------------------------
+
+%macro LUMCONVERTRANGE 4
+cglobal %1, 2, 3, 3, dst, width, x
+    movsxdifnidn widthq, widthd
+    xor              xq, xq
+    mova             m4, [%2]
+    mova             m5, [%3]
+.loop:
+    pmovsxwd         m0, [dstq+xq*2]
+    pmovsxwd         m1, [dstq+xq*2+mmsize/2]
+    pmulld           m0, m4
+    pmulld           m1, m4
+    paddd            m0, m5
+    paddd            m1, m5
+    psrad            m0, %4
+    psrad            m1, %4
+    packssdw         m0, m0
+    packssdw         m1, m1
+    movq    [dstq+xq*2], m0
+    movq    [dstq+xq*2+mmsize/2], m1
+    add              xq, mmsize / 2
+    cmp              xd, widthd
+    jl .loop
+    RET
+%endmacro
+
+;-----------------------------------------------------------------------------
+; chrConvertRange
+;
+; void ff_chrRangeToJpeg_<opt>(int16_t *dstU, int16_t *dstV, int width);
+; void ff_chrRangeFromJpeg_<opt>(int16_t *dstU, int16_t *dstV, int width);
+;
+;-----------------------------------------------------------------------------
+
+%macro CHRCONVERTRANGE 4
+cglobal %1, 3, 4, 4, dstU, dstV, width, x
+    movsxdifnidn widthq, widthd
+    xor              xq, xq
+    mova             m4, [%2]
+    mova             m5, [%3]
+.loop:
+    pmovsxwd         m0, [dstUq+xq*2]
+    pmovsxwd         m1, [dstUq+xq*2+mmsize/2]
+    pmovsxwd         m2, [dstVq+xq*2]
+    pmovsxwd         m3, [dstVq+xq*2+mmsize/2]
+    pmulld           m0, m4
+    pmulld           m1, m4
+    pmulld           m2, m4
+    pmulld           m3, m4
+    paddd            m0, m5
+    paddd            m1, m5
+    paddd            m2, m5
+    paddd            m3, m5
+    psrad            m0, %4
+    psrad            m1, %4
+    psrad            m2, %4
+    psrad            m3, %4
+    packssdw         m0, m0
+    packssdw         m1, m1
+    packssdw         m2, m2
+    packssdw         m3, m3
+    movq   [dstUq+xq*2], m0
+    movq   [dstUq+xq*2+mmsize/2], m1
+    movq   [dstVq+xq*2], m2
+    movq   [dstVq+xq*2+mmsize/2], m3
+    add              xq, mmsize / 2
+    cmp              xd, widthd
+    jl .loop
+    RET
+%endmacro
+
+%if ARCH_X86_64
+INIT_XMM sse4
+LUMCONVERTRANGE lumRangeToJpeg,   lum_to_mult,   lum_to_offset,   lum_to_shift
+CHRCONVERTRANGE chrRangeToJpeg,   chr_to_mult,   chr_to_offset,   chr_to_shift
+LUMCONVERTRANGE lumRangeFromJpeg, lum_from_mult, lum_from_offset, lum_from_shift
+CHRCONVERTRANGE chrRangeFromJpeg, chr_from_mult, chr_from_offset, chr_from_shift
+%endif
