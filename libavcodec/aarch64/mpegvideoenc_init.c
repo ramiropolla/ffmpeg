d94d747f39d12f3e09fac491af2b869fdca7550b
Ramiro Polla <ramiro.polla@gmail.com>
2024-08-27 17:04:38 +0200

    don't export inv_zigzag_direct16

--- libavcodec/aarch64/mpegvideoenc_init.c
+++ libavcodec/aarch64/mpegvideoenc_init.c
@@ -24,18 +24,6 @@
 #include "libavutil/aarch64/cpu.h"
 #include "libavcodec/mpegvideoenc.h"
 
-/* not permutated inverse zigzag_direct + 1 for NEON quantizer */
-DECLARE_ALIGNED(16, const uint16_t, ff_inv_zigzag_direct16)[64] = {
-    1,  2,  6,  7,  15, 16, 28, 29,
-    3,  5,  8,  14, 17, 27, 30, 43,
-    4,  9,  13, 18, 26, 31, 42, 44,
-    10, 12, 19, 25, 32, 41, 45, 54,
-    11, 20, 24, 33, 40, 46, 53, 55,
-    21, 23, 34, 39, 47, 52, 56, 61,
-    22, 35, 38, 48, 51, 57, 60, 62,
-    36, 37, 49, 50, 58, 59, 63, 64,
-};
-
 int ff_quantize_neon(int16_t *temp_block, int16_t *block,
                      const int16_t *qmat, const int16_t *bias,
                      int *max_qcoeff, int last_non_zero_p1);
