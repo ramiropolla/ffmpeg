2ad7e04bfdf0563ceb9117a6c5386838165c98e2
Ramiro Polla <ramiro.polla@gmail.com>
2024-09-18 23:46:06 +0200

    [TODO retest] swscale/range_convert: fix mpeg ranges in yuv range conversion for non-8-bit pixel formats
    There is an issue with the constants used in YUV to YUV range conversion,
    where the upper bound is not respected when converting to mpeg range.
    
    With this commit, the constants are calculated at runtime, depending on
    the bit depth. This approach also allows us to more easily understand how
    the constants are derived.
    
    For bit depths <= 14, the number of fixed point bits has been set to 14
    for all conversions, to simplify the code.
    For bit depths > 14, the number of fixed points bits has been raised and
    set to 18, to allow for the conversion to be accurate enough for the mpeg
    range to be respected.
    
    The convert functions now take the conversion constants (coeff and offset)
    as function arguments.
    For bit depths <= 14, offset is 32-bit.
    For bit depths > 14, offset is 64-bit.
    
    x86_64:
    chrRangeFromJpeg8_1920_c:    5804.5  5845.2 ( 0.99x)
    chrRangeFromJpeg16_1920_c:   5792.8  5809.1 ( 1.00x)
    chrRangeToJpeg8_1920_c:      9388.6  9462.2 ( 0.99x)
    chrRangeToJpeg16_1920_c:     5796.5  9261.5 ( 0.63x)
    lumRangeFromJpeg8_1920_c:    4147.9  4191.4 ( 0.99x)
    lumRangeFromJpeg16_1920_c:   4529.0  4143.4 ( 1.09x)
    lumRangeToJpeg8_1920_c:      5694.1  5720.5 ( 1.00x)
    lumRangeToJpeg16_1920_c:     5334.2  5139.5 ( 1.04x)
    
    aarch64 A55:
    chrRangeFromJpeg8_1920_c:   28833.8 28834.8 ( 1.00x)
    chrRangeFromJpeg16_1920_c:  28842.8 28840.6 ( 1.00x)
    chrRangeToJpeg8_1920_c:     23070.6 23072.5 ( 1.00x)
    chrRangeToJpeg16_1920_c:    17313.8 23075.1 ( 0.75x)
    lumRangeFromJpeg8_1920_c:   15388.1 15386.7 ( 1.00x)
    lumRangeFromJpeg16_1920_c:  15388.0 15383.8 ( 1.00x)
    lumRangeToJpeg8_1920_c:     19226.2 19223.6 ( 1.00x)
    lumRangeToJpeg16_1920_c:    19225.5 19225.5 ( 1.00x)
    
    aarch64 A76:
    chrRangeFromJpeg8_1920_c:    6317.8  6318.5 ( 1.00x)
    chrRangeFromJpeg16_1920_c:   6322.9  6323.5 ( 1.00x)
    chrRangeToJpeg8_1920_c:      9287.1  9170.0 ( 1.01x)
    chrRangeToJpeg16_1920_c:     6104.9  9195.6 ( 0.66x)
    lumRangeFromJpeg8_1920_c:    4359.1  4425.5 ( 0.98x)
    lumRangeFromJpeg16_1920_c:   4358.8  4436.8 ( 0.98x)
    lumRangeToJpeg8_1920_c:      5957.2  6017.2 ( 0.99x)
    lumRangeToJpeg16_1920_c:     6072.5  6017.2 ( 1.01x)
    
    NOTE: all simd optimizations for range_convert have been disabled.
          they will be re-enabled when they are fixed for each architecture.
    
    NOTE2: the same issue still exists in rgb2yuv conversions, which is not
           addressed in this commit.

--- tests/checkasm/sw_range_convert.c
+++ tests/checkasm/sw_range_convert.c
@@ -65,8 +65,11 @@ static void check_lumConvertRange(int from)
 
     LOCAL_ALIGNED_32(int16_t, dst0, [LARGEST_INPUT_SIZE * 2]);
     LOCAL_ALIGNED_32(int16_t, dst1, [LARGEST_INPUT_SIZE * 2]);
+    int32_t *dst0_32 = (int32_t *) dst0;
+    int32_t *dst1_32 = (int32_t *) dst1;
 
-    declare_func(void, int16_t *dst, int width);
+    declare_func(void, int16_t *dst, int width,
+                       int coeff, int64_t offset);
 
     sws = sws_alloc_context();
     if (sws_init_context(sws, NULL, NULL) < 0)
@@ -81,6 +84,10 @@ static void check_lumConvertRange(int from)
         const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(pix_fmt);
         int bit_depth = desc->comp[0].depth;
         int sample_size = bit_depth == 16 ? sizeof(int32_t) : sizeof(int16_t);
+        int src_shift = bit_depth <= 14 ? 15 - bit_depth : 19 - bit_depth;
+        int mpeg_min = 16 << (bit_depth - 8);
+        int mpeg_max = 235 << (bit_depth - 8);
+        int jpeg_max = (1 << bit_depth) - 1;
         c->srcFormat = pix_fmt;
         c->dstFormat = pix_fmt;
         c->dstBpc = bit_depth;
@@ -89,12 +96,34 @@ static void check_lumConvertRange(int from)
             int width = input_sizes[dstWi];
             if (check_func(c->lumConvertRange, "%s%d_%d", func_str, bit_depth, width)) {
                 randomize_buffers(dst0, dst1, bit_depth, width);
-                call_ref(dst0, width);
-                call_new(dst1, width);
+                if (!from) {
+                    if (bit_depth == 16) {
+                        dst1_32[0] = dst0_32[0] = mpeg_min << src_shift;
+                        dst1_32[1] = dst0_32[1] = mpeg_max << src_shift;
+                    } else {
+                        dst1[0] = dst0[0] = mpeg_min << src_shift;
+                        dst1[1] = dst0[1] = mpeg_max << src_shift;
+                    }
+                }
+                call_ref(dst0, width,
+                         c->lumConvertRange_coeff, c->lumConvertRange_offset);
+                call_new(dst1, width,
+                         c->lumConvertRange_coeff, c->lumConvertRange_offset);
                 if (memcmp(dst0, dst1, width * sample_size))
                     fail();
+                if (!from) {
+                    /* check that the mpeg range is respected */
+                    if (bit_depth == 16) {
+                        if ((dst1_32[0] >> src_shift) > 0 || (dst1_32[1] >> src_shift) != jpeg_max)
+                            fail();
+                    } else {
+                        if ((dst1[0] >> src_shift) > 0 || (dst1[1] >> src_shift) != jpeg_max)
+                            fail();
+                    }
+                }
                 if (width == LARGEST_INPUT_SIZE && (bit_depth == 8 || bit_depth == 16))
-                    bench_new(dst1, width);
+                    bench_new(dst1, width,
+                              c->lumConvertRange_coeff, c->lumConvertRange_offset);
             }
         }
     }
@@ -115,8 +144,11 @@ static void check_chrConvertRange(int from)
     LOCAL_ALIGNED_32(int16_t, dstV0, [LARGEST_INPUT_SIZE * 2]);
     LOCAL_ALIGNED_32(int16_t, dstU1, [LARGEST_INPUT_SIZE * 2]);
     LOCAL_ALIGNED_32(int16_t, dstV1, [LARGEST_INPUT_SIZE * 2]);
+    int32_t *dstU0_32 = (int32_t *) dstU0;
+    int32_t *dstU1_32 = (int32_t *) dstU1;
 
-    declare_func(void, int16_t *dstU, int16_t *dstV, int width);
+    declare_func(void, int16_t *dstU, int16_t *dstV, int width,
+                       int coeff, int64_t offset);
 
     sws = sws_alloc_context();
     if (sws_init_context(sws, NULL, NULL) < 0)
@@ -131,6 +163,10 @@ static void check_chrConvertRange(int from)
         const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(pix_fmt);
         int bit_depth = desc->comp[0].depth;
         int sample_size = bit_depth == 16 ? sizeof(int32_t) : sizeof(int16_t);
+        int src_shift = bit_depth <= 14 ? 15 - bit_depth : 19 - bit_depth;
+        int mpeg_min = 16 << (bit_depth - 8);
+        int mpeg_max = 240 << (bit_depth - 8);
+        int jpeg_max = (1 << bit_depth) - 1;
         c->srcFormat = pix_fmt;
         c->dstFormat = pix_fmt;
         c->dstBpc = bit_depth;
@@ -140,13 +176,35 @@ static void check_chrConvertRange(int from)
             if (check_func(c->chrConvertRange, "%s%d_%d", func_str, bit_depth, width)) {
                 randomize_buffers(dstU0, dstU1, bit_depth, width);
                 randomize_buffers(dstV0, dstV1, bit_depth, width);
-                call_ref(dstU0, dstV0, width);
-                call_new(dstU1, dstV1, width);
+                if (!from) {
+                    if (bit_depth == 16) {
+                        dstU1_32[0] = dstU0_32[0] = mpeg_min << src_shift;
+                        dstU1_32[1] = dstU0_32[1] = mpeg_max << src_shift;
+                    } else {
+                        dstU1[0] = dstU0[0] = mpeg_min << src_shift;
+                        dstU1[1] = dstU0[1] = mpeg_max << src_shift;
+                    }
+                }
+                call_ref(dstU0, dstV0, width,
+                         c->chrConvertRange_coeff, c->chrConvertRange_offset);
+                call_new(dstU1, dstV1, width,
+                         c->chrConvertRange_coeff, c->chrConvertRange_offset);
                 if (memcmp(dstU0, dstU1, width * sample_size) ||
                     memcmp(dstV0, dstV1, width * sample_size))
                     fail();
+                if (!from) {
+                    /* check that the mpeg range is respected */
+                    if (bit_depth == 16) {
+                        if ((dstU1_32[0] >> src_shift) > 0 || (dstU1_32[1] >> src_shift) != jpeg_max)
+                            fail();
+                    } else {
+                        if ((dstU1[0] >> src_shift) > 0 || (dstU1[1] >> src_shift) != jpeg_max)
+                            fail();
+                    }
+                }
                 if (width == LARGEST_INPUT_SIZE && (bit_depth == 8 || bit_depth == 16))
-                    bench_new(dstU1, dstV1, width);
+                    bench_new(dstU1, dstV1, width,
+                              c->chrConvertRange_coeff, c->chrConvertRange_offset);
             }
         }
     }
