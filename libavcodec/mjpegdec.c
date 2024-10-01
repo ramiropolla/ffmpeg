55bab9404ffebab91f8bc42d5b8604613a08a3c6
Ramiro Polla <ramiro.polla@gmail.com>
2024-10-01 20:33:50 +0200

    avcodec/mjpegdec: fix skipping of bytes for unknown APPx markers
    The loop to skip the remaining bytes was off by one for all markers
    except for Adob.
    
    This patch uses post-decrement instead of pre-decrement in the while
    loop to make the len value easier to understand, and updates the len
    value to reflect this change for the Adob marker.

--- libavcodec/mjpegdec.c
+++ libavcodec/mjpegdec.c
@@ -1935,7 +1935,7 @@ static int mjpeg_decode_app(MJpegDecodeContext *s)
     }
 
     if (   id == AV_RB32("Adob")
-        && len >= 7
+        && len >= 8
         && show_bits(&s->gb, 8) == 'e'
         && show_bits_long(&s->gb, 32) != AV_RB32("e_CM")) {
         skip_bits(&s->gb,  8); /* 'e' */
@@ -1945,7 +1945,7 @@ static int mjpeg_decode_app(MJpegDecodeContext *s)
         s->adobe_transform = get_bits(&s->gb,  8);
         if (s->avctx->debug & FF_DEBUG_PICT_INFO)
             av_log(s->avctx, AV_LOG_INFO, "mjpeg: Adobe header found, transform=%d\n", s->adobe_transform);
-        len -= 7;
+        len -= 8;
         goto out;
     }
 
@@ -2166,7 +2166,7 @@ out:
     if (len < 0)
         av_log(s->avctx, AV_LOG_ERROR,
                "mjpeg: error, decode_app parser read over the end\n");
-    while (--len > 0)
+    while (len-- > 0)
         skip_bits(&s->gb, 8);
 
     return 0;
