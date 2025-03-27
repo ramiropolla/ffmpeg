cff120e760e3d494d08ebb31b8958760811e38f0
Ramiro Polla <ramiro.polla@gmail.com>
2025-03-27 19:01:22 +0100

    [WIP] asmjit neon

--- libswscale/ops_internal.h
+++ libswscale/ops_internal.h
@@ -149,7 +149,7 @@ int ff_sws_ops_compile_backend(void *logctx, const SwsOpBackend *backend,
  *
  * Returns 0 on success, or a negative error code on failure.
  */
-int ff_sws_ops_compile(void *logctx, const SwsOpList *ops, SwsOpChain *chain);
+int ff_sws_ops_compile(SwsContext *logctx, const SwsOpList *ops, SwsOpChain *chain);
 
 /**
  * Set of helpers for writing backends based on static function tables.
