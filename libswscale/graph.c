b2358e22879511c3b3839225af904167ed65e1ac
Ramiro Polla <ramiro.polla@gmail.com>
2025-03-31 02:43:44 +0200

    PROBABLE BUGFIX: don't memcpy when both pointers are equal

--- libswscale/graph.c
+++ libswscale/graph.c
@@ -105,7 +105,9 @@ static void run_copy(const SwsImg *out_base, const SwsImg *in_base,
         const int lines = h >> ff_fmt_vshift(in.fmt, i);
         av_assert1(in.data[i]);
 
-        if (in.linesize[i] == out.linesize[i]) {
+        if (out.data[i] == in.data[i]) {
+            /* noop */
+        } else if (in.linesize[i] == out.linesize[i]) {
             memcpy(out.data[i], in.data[i], lines * out.linesize[i]);
         } else {
             const int linesize = FFMIN(out.linesize[i], in.linesize[i]);
