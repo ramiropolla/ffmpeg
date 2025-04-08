2c83a10f7bff77a75c20009d69b7ce4fe1a80260
Niklas Haas <git@haasn.dev>
2025-04-08 20:44:41 +0200

    fixup x86

--- libswscale/x86/ops.c
+++ libswscale/x86/ops.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <float.h>
+
 #include "../ops_internal.h"
 
 #define DECL_ASM(TYPE, NAME, ...)                                               \
