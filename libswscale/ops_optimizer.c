0c47b30a6b7bef9edfe4964a7115b7b2f3053a45
Ramiro Polla <ramiro.polla@gmail.com>
2025-05-04 20:31:07 +0200

    PROBABLE BUGFIX: don't drop crucial component unpacking information when optimizing

--- libswscale/ops_optimizer.c
+++ libswscale/ops_optimizer.c
@@ -474,9 +474,11 @@ retry:
                 goto retry;
             }
 
+#if 0
             /* Skip unpacking components that are not used */
             for (int i = 3; i > 0 && next->comps.unused[i]; i--)
                 op->pack.pattern[i] = 0;
+#endif
             break;
 
         case SWS_OP_PACK:
