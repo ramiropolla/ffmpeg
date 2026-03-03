585e8ccea4dd6c8e12808652356613114a044c68
Ramiro Polla <ramiro.polla@gmail.com>
2026-03-03 12:28:08 +0100

    include SSIM as part of the normal log
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Ramiro Polla <ramiro.polla@gmail.com>

--- libswscale/tests/swscale.c
+++ libswscale/tests/swscale.c
@@ -242,21 +242,22 @@ static void print_test(enum AVPixelFormat src_fmt, enum AVPixelFormat dst_fmt,
                        float loss_ref, float expected_loss, float loss,
                        int64_t time_ref, int64_t time)
 {
-    av_log(NULL, AV_LOG_INFO, "%s %dx%d -> %s %3dx%3d, flags=0x%x dither=%u\n",
+    av_log(NULL, AV_LOG_INFO, "%s %dx%d -> %s %3dx%3d, flags=0x%x dither=%u"
+           " SSIM {Y=%f U=%f V=%f A=%f}\n",
            av_get_pix_fmt_name(src_fmt), src->width, src->height,
            av_get_pix_fmt_name(dst_fmt), dst->width, dst->height,
-           mode->flags, mode->dither);
-
-    av_log(NULL, AV_LOG_VERBOSE - 4, "  SSIM {Y=%f U=%f V=%f A=%f}\n",
+           mode->flags, mode->dither,
            ssim[0], ssim[1], ssim[2], ssim[3]);
 
     if (loss - expected_loss > 1e-4 && dst_w >= ref->width && dst_h >= ref->height) {
         const int bad = loss - expected_loss > 1e-2;
         const int level = bad ? AV_LOG_ERROR : AV_LOG_WARNING;
+#if 1
         av_log(NULL, level, "%s %dx%d -> %s %3dx%3d, flags=0x%x dither=%u\n",
                av_get_pix_fmt_name(src_fmt), src->width, src->height,
                av_get_pix_fmt_name(dst_fmt), dst->width, dst->height,
                mode->flags, mode->dither);
+#endif
         av_log(NULL, level, "  loss %g is %s by %g, expected loss %g\n",
                loss, bad ? "WORSE" : "worse", loss - expected_loss, expected_loss);
     }
