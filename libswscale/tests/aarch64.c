c4925d41475da2c6de5d8f348be82216f5503458
Ramiro Polla <ramiro.polla@gmail.com>
2026-03-11 21:26:12 +0100

    ok

--- libswscale/tests/aarch64.c
+++ libswscale/tests/aarch64.c
@@ -6,27 +6,29 @@ int main(int argc, char *argv[])
 {
     AArch64Context *actx = aarch64_alloc();
 
-    AArch64Op x0 = aarch64_x0();
-    AArch64Op x1 = aarch64_x1();
-    AArch64Op x2 = aarch64_x2();
+    AArch64Op x0 = a64op_gpx(0);
+    AArch64Op x1 = a64op_gpx(1);
+    AArch64Op x2 = a64op_gpx(2);
+    AArch64Op v2 = a64op_vec(2);
+    AArch64Op v3 = a64op_vec(3);
 
-    aarch64_comment(actx, "apfelstrudel");
+    aarch64_add_comment(actx, "apfelstrudel");
 
     /* GPR instructions */
-    aarch64_add(actx, x0, x1, x2); aarch64_annotate(actx, "something");
-    aarch64_mov(actx, x0, x1);
+    a64insn_add(actx, x0, x1, x2); aarch64_annotate(actx, "something");
+    a64insn_mov(actx, x0, x1);
 
     /* vector with arrangement specifier */
-    aarch64_fadd(actx,  aarch64_vec4s(0), aarch64_vec4s(1), aarch64_vec4s(2));
-    aarch64_ushr(actx,  aarch64_vec8h(3), aarch64_vec8h(4), aarch64_gpx(5));
-    aarch64_rev16(actx, aarch64_vec16b(0), aarch64_vec16b(1));
+    a64insn_fadd(actx,  a64op_vec4s(0), a64op_vec4s(1), a64op_vec4s(2));
+    a64insn_ushr(actx,  a64op_vec8h(3), a64op_vec8h(4), a64op_gpx(5));
+    a64insn_rev16(actx, a64op_vec16b(0), a64op_vec16b(1));
 
     /* scalar arrangement specifier */
-    aarch64_addv(actx, aarch64_s0(), aarch64_vec4s(1));
-    aarch64_ucvtf(actx, aarch64_d0(), aarch64_d1());
+    a64insn_addv(actx, a64op_vecs(0), a64op_vec4s(1));
+    a64insn_ucvtf(actx, a64op_vecd(0), a64op_vecd(1));
 
     /* no arrangement specifier */
-    aarch64_mov(actx, aarch64_v2(), aarch64_v3());
+    a64insn_mov(actx, v2, v3);
 
     AVBPrint bp;
     av_bprint_init(&bp, 0, AV_BPRINT_SIZE_AUTOMATIC);
