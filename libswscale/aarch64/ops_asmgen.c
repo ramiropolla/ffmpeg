3a0d8df91e05e747383b3c891ab4f9cc7d7ea6e9
Ramiro Polla <ramiro.polla@gmail.com>
2026-03-19 17:19:14 +0100

    src s->

--- libswscale/aarch64/ops_asmgen.c
+++ libswscale/aarch64/ops_asmgen.c
@@ -532,29 +532,29 @@ static void asmgen_op_convert(SwsAArch64Context *s, const SwsAArch64OpImplParams
     }
     if (s->block_size == 8) {
         if (src_el_size == 1 && dst_el_size > src_el_size) {
-            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].h8(), src_vl[i].b8());
+            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].h8(), s->vl[i].b8());
             src_el_size = 2;
         } else if (src_el_size == 4 && dst_el_size < src_el_size) {
-            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].h4(), src_vl[i].s4());
-            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vh[i].h4(), src_vh[i].s4());
+            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].h4(), s->vl[i].s4());
+            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vh[i].h4(), s->vh[i].s4());
             LOOP_MASK(s, p, i) a64insn_ins  (a, s->vl[i].d(1), s->vh[i].d(0));
             src_el_size = 2;
         }
         if (src_el_size == 2 && dst_el_size == 4) {
-            LOOP_MASK(s, p, i) a64insn_uxtl2(a, s->vh[i].s4(), src_vl[i].h8());
-            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].s4(), src_vl[i].h4());
+            LOOP_MASK(s, p, i) a64insn_uxtl2(a, s->vh[i].s4(), s->vl[i].h8());
+            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].s4(), s->vl[i].h4());
             src_el_size = 4;
         } else if (src_el_size == 2 && dst_el_size == 1) {
-            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].b8(), src_vl[i].h8());
+            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].b8(), s->vl[i].h8());
             src_el_size = 1;
         }
     } else /* if (s->block_size == 16) */ {
         if (src_el_size == 1 && dst_el_size == 2) {
-            LOOP_MASK(s, p, i) a64insn_uxtl2(a, s->vh[i].h8(), src_vl[i].b16());
-            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].h8(), src_vl[i].b8());
+            LOOP_MASK(s, p, i) a64insn_uxtl2(a, s->vh[i].h8(), s->vl[i].b16());
+            LOOP_MASK(s, p, i) a64insn_uxtl (a, s->vl[i].h8(), s->vl[i].b8());
         } else if (src_el_size == 2 && dst_el_size == 1) {
-            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].b8(), src_vl[i].h8());
-            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vh[i].b8(), src_vh[i].h8());
+            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vl[i].b8(), s->vl[i].h8());
+            LOOP_MASK(s, p, i) a64insn_xtn  (a, s->vh[i].b8(), s->vh[i].h8());
             LOOP_MASK(s, p, i) a64insn_ins  (a, s->vl[i].d(1), s->vh[i].d(0));
         }
     }
