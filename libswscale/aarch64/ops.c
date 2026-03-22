eea8f453d8c02e4a3fc22809a0075ca567d3e19e
Ramiro Polla <ramiro.polla@gmail.com>
2026-03-22 23:32:56 +0100

    ok

--- libswscale/aarch64/ops.c
+++ libswscale/aarch64/ops.c
@@ -307,8 +307,8 @@ static int aarch64_setup(SwsOpList *ops, int block_size, int n,
 /*********************************************************************/
 static int aarch64_compile(SwsContext *ctx, SwsOpList *ops, SwsCompiledOp *out)
 {
-    int ret;
     int block_size = (ff_sws_op_list_max_size(ops) == 4) ? 8 : 16;
+    int ret;
 
     const int cpu_flags = av_get_cpu_flags();
     if (!(cpu_flags & AV_CPU_FLAG_NEON))
