a923d7c5b96b82da5d873ce933679c9e53e294be
Niklas Haas <git@haasn.dev>
2026-05-01 17:50:05 +0200

    swscale/ops: make compile() take const SwsOpList *
    The old x86 backend was the only backend that actually mutated the ops list.
    With this gone, we can constify this parameter.
    
    Signed-off-by: Niklas Haas <git@haasn.dev>

--- libswscale/uops_backend.c
+++ libswscale/uops_backend.c
@@ -131,7 +131,7 @@ static void process(const SwsOpExec *exec, const void *priv,
     }
 }
 
-static int compile(SwsContext *ctx, SwsOpList *ops, SwsCompiledOp *out)
+static int compile(SwsContext *ctx, const SwsOpList *ops, SwsCompiledOp *out)
 {
     int ret;
 
