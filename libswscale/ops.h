db2c196661c084b4ba14c51f5ed04aff814da5b9
Niklas Haas <git@haasn.dev>
2026-06-02 14:39:34 +0200

    swscale/ops: generalize SwsReadWriteOp.packed to enum
    I want to start adding more data layouts, like semiplanar formats (nv12), or
    palette formats. I made an effort to distinguish existing checks for rw.packed
    into "mode != PLANAR" and "mode == PACKED", based on the intent of the
    surrounding code, in anticipation of these new layouts.
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Niklas Haas <git@haasn.dev>

--- libswscale/ops.h
+++ libswscale/ops.h
@@ -84,6 +84,11 @@ typedef struct SwsComps {
     AVRational min[4], max[4];
 } SwsComps;
 
+typedef enum SwsReadWriteMode {
+    SWS_RW_PLANAR,  /* one plane per component */
+    SWS_RW_PACKED,  /* all components on a single plane */
+} SwsReadWriteMode;
+
 typedef struct SwsReadWriteOp {
     /**
      * Examples:
@@ -93,9 +98,9 @@ typedef struct SwsReadWriteOp {
      *   monow     = 1x u8 (frac 3)
      *   rgb4      = 1x u8 (frac 1)
      */
+    SwsReadWriteMode mode; /* how data is laid out in memory */
     uint8_t elems; /* number of elements (of type `op.type`) to read/write */
     uint8_t frac;  /* fractional pixel step factor (log2) */
-    bool packed;   /* read multiple elements from a single plane */
 
     /**
      * Filter kernel to apply to each plane while sampling. Currently, only
