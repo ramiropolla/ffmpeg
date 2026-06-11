6029a94341ced984c1ebd5fc8cac8f4eab5cf5aa
Ramiro Polla <ramiro.polla@gmail.com>
2026-06-11 22:55:19 +0200

    swscale/aarch64/ops: specialize convert and expand operations
    This is similar to the way SWS_UOP_TO_{U8,U16,U32,F32} and
    SWS_UOP_EXPAND_{PAIR,QUAD} work and will simplify the move to uops.

--- libswscale/aarch64/ops_impl_conv.c
+++ libswscale/aarch64/ops_impl_conv.c
@@ -181,7 +181,19 @@ static int convert_to_aarch64_impl(SwsContext *ctx, const SwsOpList *ops, int n,
     case SWS_OP_RSHIFT:     out->op = AARCH64_SWS_OP_RSHIFT;     break;
     case SWS_OP_CLEAR:      out->op = AARCH64_SWS_OP_CLEAR;      break;
     case SWS_OP_CONVERT:
-        out->op = op->convert.expand ? AARCH64_SWS_OP_EXPAND : AARCH64_SWS_OP_CONVERT;
+        if (op->convert.expand) {
+            switch (op->convert.to) {
+            case SWS_PIXEL_U16: out->op = AARCH64_SWS_OP_EXPAND_PAIR; break;
+            case SWS_PIXEL_U32: out->op = AARCH64_SWS_OP_EXPAND_QUAD; break;
+            }
+        } else {
+            switch (op->convert.to) {
+            case SWS_PIXEL_U8:  out->op = AARCH64_SWS_OP_TO_U8;  break;
+            case SWS_PIXEL_U16: out->op = AARCH64_SWS_OP_TO_U16; break;
+            case SWS_PIXEL_U32: out->op = AARCH64_SWS_OP_TO_U32; break;
+            case SWS_PIXEL_F32: out->op = AARCH64_SWS_OP_TO_F32; break;
+            }
+        }
         break;
     case SWS_OP_MIN:        out->op = AARCH64_SWS_OP_MIN;        break;
     case SWS_OP_MAX:        out->op = AARCH64_SWS_OP_MAX;        break;
@@ -251,10 +263,6 @@ static int convert_to_aarch64_impl(SwsContext *ctx, const SwsOpList *ops, int n,
         MASK_SET(out->mask, 1, !!op->clear.value[1].den);
         MASK_SET(out->mask, 2, !!op->clear.value[2].den);
         MASK_SET(out->mask, 3, !!op->clear.value[3].den);
-        break;
-    case AARCH64_SWS_OP_EXPAND:
-    case AARCH64_SWS_OP_CONVERT:
-        out->to_type = sws_pixel_to_aarch64(op->convert.to);
         break;
     case AARCH64_SWS_OP_LINEAR:
         /**
