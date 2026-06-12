498a53df791d378ac822ab1023a9acf42816a827
Ramiro Polla <ramiro.polla@gmail.com>
2026-06-12 21:18:35 +0200

    swscale/aarch64/ops: specialize linear operation
    This is similar to the way SWS_UOP_LINEAR{,_FMA} work and will simplify
    the move to uops.

--- libswscale/aarch64/ops_impl_conv.c
+++ libswscale/aarch64/ops_impl_conv.c
@@ -198,7 +198,11 @@ static int convert_to_aarch64_impl(SwsContext *ctx, const SwsOpList *ops, int n,
     case SWS_OP_MIN:        out->op = AARCH64_SWS_OP_MIN;        break;
     case SWS_OP_MAX:        out->op = AARCH64_SWS_OP_MAX;        break;
     case SWS_OP_SCALE:      out->op = AARCH64_SWS_OP_SCALE;      break;
-    case SWS_OP_LINEAR:     out->op = AARCH64_SWS_OP_LINEAR;     break;
+    case SWS_OP_LINEAR:
+        out->op = (ctx->flags & SWS_BITEXACT)
+                ? AARCH64_SWS_OP_LINEAR
+                : AARCH64_SWS_OP_LINEAR_FMA;
+        break;
     case SWS_OP_DITHER:     out->op = AARCH64_SWS_OP_DITHER;     break;
     case SWS_OP_FILTER_H:
     case SWS_OP_FILTER_V:
@@ -265,6 +269,7 @@ static int convert_to_aarch64_impl(SwsContext *ctx, const SwsOpList *ops, int n,
         MASK_SET(out->mask, 3, !!op->clear.value[3].den);
         break;
     case AARCH64_SWS_OP_LINEAR:
+    case AARCH64_SWS_OP_LINEAR_FMA:
         /**
          * The out->linear.mask field packs the 4x5 matrix from SwsLinearOp as
          * 2 bits per element:
