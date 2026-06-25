2babd978d3769fd66b57b947f9d869cedc19804e
Ramiro Polla <ramiro.polla@gmail.com>
2026-06-25 23:45:53 +0200

    swscale/jit: implement ff_sws_jit_protect() for win32
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Ramiro Polla <ramiro.polla@gmail.com>

--- libswscale/jit.c
+++ libswscale/jit.c
@@ -70,6 +70,9 @@ void *ff_sws_jit_alloc(size_t size)
 
 int ff_sws_jit_protect(void *ptr, size_t size)
 {
+    DWORD old_protect;
+    if (!VirtualProtect(ptr, size, PAGE_EXECUTE_READ, &old_protect))
+        return AVERROR(EINVAL);
     return 0;
 }
 
