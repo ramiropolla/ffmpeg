727c6a807493f5bb6b9d1b4fb2fd5d6507de1b22
Ramiro Polla <ramiro.polla@gmail.com>
2026-07-03 17:38:52 +0200

    swscale/aarch64/ops: export convert_to_aarch64_impl() to ff_sws_aarch64_ops_translate()
    This function will be used by both the CPS and JIT aarch64 backends.
    
    Note that the function still needs to be #include'd directly when not
    targetting aarch64, since the symbol will be absent from libswscale
    itself.
    
    Sponsored-by: Sovereign Tech Fund
    Signed-off-by: Ramiro Polla <ramiro.polla@gmail.com>

--- libswscale/tests/sws_ops_aarch64.c
+++ libswscale/tests/sws_ops_aarch64.c
@@ -30,7 +30,11 @@
 #include "libswscale/op_list_gen_template.c"
 #include "libswscale/ops_dispatch.h"
 
+#if ARCH_AARCH64
+#include "libswscale/aarch64/ops_impl.h"
+#else
 #include "libswscale/aarch64/ops_impl_conv.c"
+#endif
 
 #ifdef _WIN32
 #include <io.h>
@@ -197,7 +201,7 @@ static int collect_ops_compile(SwsContext *ctx, const SwsOpList *ops,
 
     for (int i = 0; i < ops->num_ops; i++) {
         SwsAArch64OpImplParams params = { 0 };
-        ret = convert_to_aarch64_impl(ctx, ops, i, block_size, &params);
+        ret = ff_sws_aarch64_ops_translate(ctx, ops, i, block_size, &params);
         if (ret == AVERROR(ENOTSUP))
             continue;
         if (ret < 0)
