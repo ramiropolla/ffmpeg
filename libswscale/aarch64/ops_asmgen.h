deff9ec95292fe7a3ee2608f8149a457f034dfb8
Ramiro Polla <ramiro.polla@gmail.com>
2026-07-06 23:59:58 +0200

    [WIP] split asmgen into static

---
+++ libswscale/aarch64/ops_asmgen.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2026 Ramiro Polla
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef SWSCALE_AARCH64_OPS_ASMGEN_H
+#define SWSCALE_AARCH64_OPS_ASMGEN_H
+
+/*********************************************************************/
+typedef struct SwsAArch64OpRegs {
+    RasmOp vl[4]; /* input/output vector registers (low bank) */
+    RasmOp vh[4]; /* input/output vector registers (high bank) */
+    RasmOp vt[8]; /* temp vector registers */
+    RasmOp vk[4]; /* constant data (may be gprs) */
+} SwsAArch64OpRegs;
+
+/*********************************************************************/
+typedef struct SwsAArch64Context {
+    RasmContext *rctx;
+
+    /* SwsOpFunc arguments. */
+    RasmOp exec;
+    RasmOp impl;
+    RasmOp bx_start;
+    RasmOp y_start;
+    RasmOp bx_end;
+    RasmOp y_end;
+
+    /* Loop iterator variables. */
+    RasmOp bx;
+    RasmOp y;
+
+    /* Scratch registers. */
+    RasmOp tmp0;
+    RasmOp tmp1;
+
+    /* CPS-related variables. */
+    RasmOp op0_func;
+    RasmOp op1_impl;
+    RasmOp cont;
+    RasmNode *load_cont_node;
+    RasmOp impl_priv;
+    SwsAArch64OpRegs regs;
+
+    /* Read/Write data pointers and padding. */
+    RasmOp in[4];
+    RasmOp out[4];
+    RasmOp in_bump[4];
+    RasmOp out_bump[4];
+
+    /* Vector register dimensions. */
+    size_t el_size;
+    size_t el_count;
+    size_t vec_size;
+    bool use_vh;
+} SwsAArch64Context;
+
+#endif /* SWSCALE_AARCH64_OPS_ASMGEN_H */
